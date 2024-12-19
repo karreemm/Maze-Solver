4cd46a9dac1c803eefb0a9b745075ee6ab292133
kareem <kareem.abdelnabi.22@gmail.com>
2024-12-19 17:08:19 +0200

    enhance OurQueue

--- ourQueue.py
+++ ourQueue.py
@@ -1,27 +1,38 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
 class OurQueue:
     def __init__(self):
-        self.queue = []
+        self.head = None  
+        self.tail = None  
+        self.length = 0   
 
     def add(self, item):
-        self.queue.append(item)
+        new_node = Node(item)
+        if self.is_empty():
+            self.head = self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1 
 
     def pop(self):
-        if not self.is_empty():
-            return self.queue.pop(0)
-        return "Queue is empty"
+        if self.is_empty():
+            return "Queue is empty"
+        popped_value = self.head.value
+        self.head = self.head.next
+        if self.head is None: 
+            self.tail = None
+        self.length -= 1
+        return popped_value
 
     def is_empty(self):
-        return len(self.queue) == 0
+        return self.head is None
 
     def size(self):
-        return len(self.queue)
+        return self.length
+    
 
-    def clear(self):
-        self.queue = []
-        return "Queue cleared"
 
-    def print_queue(self):
-        if not self.is_empty():
-            print("Queue:", self.queue)
-        else:
-            print("Queue is empty")
